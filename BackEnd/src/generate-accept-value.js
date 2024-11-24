a0562e802d6c53ea371ac0eb6c3bf7c273bc681a
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-11-24 10:48:47 +0600

    🩹 use template literal

--- BackEnd/src/generate-accept-value.js
+++ BackEnd/src/generate-accept-value.js
@@ -6,7 +6,7 @@ function generateAcceptValue(acceptKey) {
     crypto
       .createHash("sha1")
       // this magic string key is actually in the spec
-      .update(acceptKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11", "binary")
+      .update(`${acceptKey}258EAFA5-E914-47DA-95CA-C5AB0DC85B11`, "binary")
       .digest("base64")
   );
 }
